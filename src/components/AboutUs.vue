fbf8d0a48acbfe02105742fa25ae90faac3c22e9
betoab728 <betoab728@gmail.com>
2025-05-18 08:41:09 -0500

    aumento de contenido

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,93 +1,94 @@
 <template>
   <section class="story" id="about">
-            <div class="story__container">
-                <div class="story--main">
-                    <h3>Descubre</h3>
-                    <h1>Nuestra historia</h1>
-                    <p>Todo comenzó con un simple objetivo: hornear el pan perfecto cada mañana. Lo que empezó como un pequeño horno y una gran pasión, hoy es Panadería Delicias, donde cada crujido cuenta una historia y cada aroma te hace sentir en casa.
-
-                    </p>
-                    <button class="button--story button">Ver más</button>
-                </div>
-                <img src="/images/about-img.jpg" alt="Some Food" class="story--image">
-            </div>     
- </section>
-        
-
+    <div class="story__container">
+      <div class="story--main">
+        <h3>Descubre</h3>
+        <h1>Nuestra historia</h1>
+        <p>
+          Todo comenzó con un simple objetivo: hornear el pan perfecto cada mañana. Lo que empezó como un pequeño horno y una gran pasión, hoy es Panadería Delicias, donde cada crujido cuenta una historia y cada aroma te hace sentir en casa.
+        </p>
+        <button class="button--story button">Ver más</button>
+      </div>
+      <img src="/images/about-img.jpg" alt="Some Food" class="story--image" />
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
-<style>
-
-
+<style scoped>
 .story {
-  height: 535.2px;
-  display: -webkit-box;
-  display: -ms-flexbox;
+  padding: 2rem 1rem;
   display: flex;
-  -webkit-box-align: center;
-      -ms-flex-align: center;
-          align-items: center;
-  -webkit-box-pack: center;
-      -ms-flex-pack: center;
-          justify-content: center;
+  justify-content: center;
+  align-items: center;
 }
 
 .story__container {
-  width: 95%;
-  height: 80%;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-align: center;
-      -ms-flex-align: center;
-          align-items: center;
-  -webkit-box-pack: center;
-      -ms-flex-pack: center;
-          justify-content: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  max-width: 1200px;
+  width: 100%;
 }
 
-.story__container .story--main {
-  width: 40%;
-  padding-right: 20px;
+.story--main {
+  flex: 1 1 300px;
+  max-width: 500px;
 }
 
-.story__container .story--main h3 {
-  width: -webkit-max-content;
-  width: -moz-max-content;
-  width: max-content;
-  color: #8f8989;
+.story--main h3 {
   text-transform: uppercase;
+  color: #8f8989;
   font-family: 'Roboto Condensed', sans-serif;
   font-weight: 700;
 }
 
-.story__container .story--main h1 {
-  width: -webkit-max-content;
-  width: -moz-max-content;
-  width: max-content;
+.story--main h1 {
+  font-size: 2rem;
 }
 
-.story__container .story--main p {
-  width: 100%;
+.story--main p {
+  font-size: 1rem;
   color: #575555;
-  font-size: 15px;
 }
 
-.story__container .story--main .button--story {
-  margin-top: 13px;
+.button--story {
+  margin-top: 1rem;
   color: #c69963;
   border: 2px solid #c69963;
-  font-size: 15px;
+  font-size: 1rem;
   font-weight: 700;
+  padding: 0.5rem 1rem;
+  background: transparent;
 }
 
-.story__container .story--image {
-  max-height: 100%;
+.story--image {
+  flex: 1 1 300px;
+  max-width: 500px;
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 8px;
 }
 
+/* Responsive adjustments */
+@media (max-width: 768px) {
+  .story__container {
+    flex-direction: column;
+    text-align: center;
+  }
 
+  .story--main,
+  .story--image {
+    max-width: 100%;
+  }
+
+  .story--image {
+    order: -1;
+  }
+}
 </style>
